7c186f6c404278bfe4f2d6bf4140c0b8eacb3b4d
jackievivianv <145908828+jackievivianv@users.noreply.github.com>
2023-12-03 18:44:48 +0000

    to do list casi terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,56 +4,58 @@ import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+let count = 0;
 const Home = () => {
 
 	const [task, setTask] = useState("");
 	const [list, setList] = useState([]);
-	const [remove, setRemove] = useState([]);
+	const [visible, setVisibleItem] = useState("none");
+	
 
+	function mouseHoverEvent(){
+			setVisibleItem("flex");    //hace visible la x
+	}
+	
+	function mouseLeaveEvent(){
+		setVisibleItem("none");       //hace invisible la x
+	}
+	
+	
 	function handleTask(event){
 		setTask(event.target.value);
 	}
+
+	function handleRemoveItem(index) {
+		setList((prevList) => prevList.filter((_, i) => i !== index));
+		count--;
+	  }
 
 	function handleList(event){
 			if (event.keyCode === 13) {
 				const newData = [...list, event.target.value]; // Crea un nuevo array con el dato adicional
 				setList(newData); // Actualiza el estado con el nuevo array
 				console.log(list);
+				count++
 				event.target.value = '';
 			}
 		
 	}
 
-	//function handleRemove(event){
-		
-	//	setRemove(list.filter(a => a !== event.target.value))
-		//console.log("newList")
-		//list.removeChild();
-	//}
-
-
-		
-		// Eliminar tarea
-		
-		//	function removeTask(listItem) {
-		//	  taskList.removeChild(listItem);
-		   
-		//	}
 
 
 	return (
-		<div className="container d-block justify-content-center bg-black p-5" style={{width: "30rem"}}>
-			<h1 className="text-center text-light p-2">My To-Do List</h1>
+		<div className="container d-block bg-black p-5 col-lg-6 col-md-4 col-sm-10 mt-5 pb-3" style={{width: "30rem"}}>
+			<h1 className="text-light p-2 d-flex justify-content-center">My To-Do List</h1>
 			<input type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm p-5" onChange={handleTask} onKeyUp={handleList} placeholder="Add your task for today here :)"></input>
 			<ul className="text-light p-3">
 				{list.map((item, index) => (
-         			 <li key={index} style={{listStyleType: "none"}} className="bg-dark p-2">
+         			 <li key={index} style={{listStyleType: "none"}} className="bg-dark p-2 border border-secondary" onMouseEnter={mouseHoverEvent} onMouseLeave={mouseLeaveEvent}>
             		{item}
-					<span onClick={() => {setList(list.filter((_, i) => i !== index))}} className="d-flex justify-content-end"> x</span>
+					<span onClick={() => handleRemoveItem(index)} className="justify-content-end text-secondary" style={{display: visible}}> x</span>
           			</li>
        				 ))}
 			</ul>
-			<span className="text-sm text-light"></span>
+			<span className="text-light font-italic" style={{fontFamily: "fantasy", fontSize: "small"}}>{count} items left</span>
 		</div>
 		
 	);
